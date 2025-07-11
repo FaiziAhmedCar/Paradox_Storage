f24b23244406dcae716715bfe64ffcc41dc30619
Faizi_Ahmed <125595034+FaiziAhmedCar@users.noreply.github.com>
2025-07-12 00:03:57 +0530

    Enhance layout component with new images and links; add logo and favicon assets

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,25 +1,48 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className=" flex min-h-screen  ">
-      <section className='bg-brand p-10' >
-        <div>
+    <div className=" flex h-screen overflow-hidden">
+      <section className="hidden w-1/2 items-center justify-center bg-brand p-10 lg:flex xl:w-2/5">
+        <div className="flex max-h-[800px] max-w-[430px] flex-col justify-center space-y-7">
+          <Link href="/sign-in">
+            <Image
+              src="/logoAuth.png"
+              alt="logo"
+              width={200}
+              height={200}
+              className=" transition-all duration-500  hover:scale-150 sm:size-20 md:size-32 lg:size-40"
+            />
+          </Link>
+          <div className="space-y-5 text-white ">
+            <h1 className="h1">Manage your files the best way</h1>
+            <p className="body-1">
+              This is a place where you can store your files.
+            </p>
+          </div>
           <Image
-            src="/logo.png"
+            src="/assets/images/files-2.png"
+            alt="files"
+            width={342}
+            height={342}
+            className="transition-all duration-500 hover:rotate-12 hover:scale-110"
+          />
+        </div>
+      </section>
+      <section className="flex flex-1 flex-col items-center bg-white p-4 py-10 lg:justify-center lg:p-10 lg:py-0">
+        <div className="mb-16 lg:hidden">
+          <Image
+            src="/logo1.png"
             alt="logo"
             width={224}
             height={82}
-            className="h-auto"
-          />
-          <div className='space-y-5 text-white ' >
-            <h1 className="h1">Manage your files the best way</h1>
-            <p className="body-1" >This is a place where you can store your files.</p>
-          </div>
+            className="h-auto w-[200px] lg:w-[250px]"
+          ></Image>
         </div>
+        {children}
       </section>
-      {children}
     </div>
   );
 };
