2d6b1f0caf6af940cdbd5d215712af0deda16dc9
Faizi_Ahmed <125595034+FaiziAhmedCar@users.noreply.github.com>
2025-07-25 00:07:28 +0530

    feat: add FileUploader and Search components to the project

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Search from "./Search";
+import FileUploader from "./FileUploader";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <header className="header">
+      <Search />
+      <div className="header-wrapper">
+        <FileUploader />
+        <form action="">
+          <Button type="submit" className="sign-out-button">
+            <Image
+              src="/assets/icons/logout.svg"
+              alt="sign-out"
+              width={24}
+              height={24}
+              className="w-6"
+            />
+          </Button>
+        </form>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
